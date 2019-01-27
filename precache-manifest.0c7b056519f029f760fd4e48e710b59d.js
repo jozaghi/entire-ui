d9ee383d56d4dd01406cce47257653c3b0e2f19d
ali reza jozaghi <ar.jozaghi@hotmail.com>
2019-01-27 17:26:23 +0800

    Updates

---
+++ precache-manifest.0c7b056519f029f760fd4e48e710b59d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "709cf120da0682076409",
+    "url": "/entire-ui/static/js/runtime~main.709cf120.js"
+  },
+  {
+    "revision": "f0292b5c07a8a34ca406",
+    "url": "/entire-ui/static/js/main.f0292b5c.chunk.js"
+  },
+  {
+    "revision": "7753a71aa7bb7fe0a637",
+    "url": "/entire-ui/static/js/1.7753a71a.chunk.js"
+  },
+  {
+    "revision": "f0292b5c07a8a34ca406",
+    "url": "/entire-ui/static/css/main.696017f0.chunk.css"
+  },
+  {
+    "revision": "388c52203a14dd396248be7c395065df",
+    "url": "/entire-ui/index.html"
+  }
+];
